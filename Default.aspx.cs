481dea997e5e209bb73bef805d2b93d2e08fa7a2
Jacwei <798244224@qq.com>
2014-03-23 17:51:04 +0800

    fds

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,13 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace 例3._1
+namespace hi
 {
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Cli(object sender, EventArgs e)
+        {
+            if (Drop.SelectedIndex == 0)
+                Response.Redirect("http://www.google.com");
+            if (Drop.SelectedIndex == 1)
+                Response.Redirect("http://www.baidu.com");
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下特性集 
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("例3._1")]
+[assembly: AssemblyTitle("hi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("例3._1")]
+[assembly: AssemblyProduct("hi")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("f70b1ea7-7ed3-4275-830c-f5b1299a8f73")]
+[assembly: Guid("5dc96788-3f52-4706-94e5-4f9a7b4ba05e")]
 
 // 程序集的版本信息由下列四个值组成:
 //
